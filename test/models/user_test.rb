9871a3d68fc45fb5002349b0c6cee9ee6a846dd6
Remi Mercier <remi.merci@gmail.com>
2018-05-29 18:09:47 +0200

    Create tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new({
+      first_name: 'Happy',
+      last_name: 'The Team',
+      email: 'hi@harry.team',
+      password: 'harry2018',
+    })
+  end
+
+  test 'Should persist user with valid parameters' do
+    assert @user.save
+  end
+
+  test 'Should find persisted user in DB' do
+    @user.save
+    @persisted_user = User.find(@user.id)
+    assert_equal @user.email, @persisted_user.email
+  end
+
+  test 'User password and password confirmation should be the same' do
+    @user.password_confirmation = 'harry2081'
+    assert_not @user.valid?
+  end
 end

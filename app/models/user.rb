a18928a903016d676c68ae7be27a58d09e70fe89
giovannirescaglio <36004885+giovannirescaglio@users.noreply.github.com>
2018-05-29 16:48:10 +0200

    Update user.rb
    I removed dependent destroy on the projects through guests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :projects, through: :guests, dependent: :destroy
+  has_many :projects, through: :guests
 end

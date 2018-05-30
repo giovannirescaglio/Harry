368a59207c0dc3062eefa7dc8503feae93abe389
Antoine Goffette <a.goffette@gmail.com>
2018-05-30 17:31:47 +0200

    done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,24 @@
 class ProjectsController < ApplicationController
   def new
-    @project = Project.new()
+    @project = Project.new
   end
 
   def create
-    raise
+    @project = Project.new(project_params)
+    @project.user = current_user
+    @project.step = 1
+    @project.save
+    if @project.persisted?
+      @guest = Guest.create(user: current_user, project: @project)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :start_date, :end_date, :max_budget)
   end
 end

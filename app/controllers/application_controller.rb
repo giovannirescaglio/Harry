028d12ae052e207b08c45727b793517362ca6280
Laura Martin <laumartin4@gmail.com>
2018-05-31 16:33:38 +0200

    fixed application controller lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  include Pundit
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:picture, :first_name])
-    include Pundit
+  end
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?

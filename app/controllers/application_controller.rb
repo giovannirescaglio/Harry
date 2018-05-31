ec4e3726f033f041fbaefbf3ba1b972cdddc4e64
Laura Martin <laumartin4@gmail.com>
2018-05-31 14:19:12 +0200

    fixed the duplicated authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :authenticate_user!
     include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?

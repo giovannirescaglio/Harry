8792495675f518201c78a09d7588933d6c261992
Remi Mercier <remi.merci@gmail.com>
2018-05-31 11:52:45 +0200

    Replace former creation logic with a cleaner version

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,30 +1,38 @@
 class MoodsController < ApplicationController
+  before_action :set_project, only: [:scenery, :create]
+
   def scenery
-    @project = Project.find(params[:project_id])
-    @moods = Mood.new
+    @mood = Mood.new
   end
 
   def lifestyle
-
   end
 
   def party
-
   end
 
   def create
-    @project = Project.find(params[:project_id])
-    if params[:wild].present?
-      @moods = Mood.new(wild: params[:wild])
-    elsif params[:city].present?
-      @moods = Mood.new(city: params[:city])
+    @mood = Mood.new(mood_params)
+    @mood.project = @project
+    @mood.user = current_user
+    if @mood.save
+      redirect_to step2_project_path(@project)
+    else
+      render :scenery
     end
-    @moods.project = @project
-    @moods.user = current_user
-    @moods.save!
   end
 
   def update
     # @mood = Mood.find(params[:id])
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def mood_params
+    params.require(:mood).permit(:wild, :city, :fancy, :trashy, :clubbing, :chilling, :project_id, :user_id)
   end
 end

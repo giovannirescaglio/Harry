38f0dd3ee3839e039231c2de8a180cb10a4b4225
Remi Mercier <remi.merci@gmail.com>
2018-06-05 11:56:48 +0200

    Add welcome email method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # after_create :send_welcome_email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -9,5 +9,11 @@ class User < ApplicationRecord
   has_many :projects, through: :guests
   has_many :scenarios
   mount_uploader :picture, PhotoUploader
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
 
